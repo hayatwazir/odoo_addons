9b108abcc908530183cee4583345cc471474643d
hayatwazir <noorhayat402@gmail.com>
2025-07-23 17:39:35 +0500

    test

--- omni_print/__manifest__.py
+++ omni_print/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Omni Print1",
+    'name': "Omni Print",
     'summary': """Omni Print can simplify your print workflow with one single click, no more downloads required.""",
     'version': '1.0.2',
     'author': "Omni Byte",
